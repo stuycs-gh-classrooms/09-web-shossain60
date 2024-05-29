1f1cd74a695959789fd89c3f662d301983d53fe1
Shihab Hossain <shossain@stuy.edu>
2024-05-29 14:24:04 -0400

    + w38

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -3,12 +3,6 @@ print('Content-type: text/html\n')
 
 import random
 
-# Printing some text
-print("<html><body>")
-print("<h1>Welcome to our random number generator page!</h1>")
-
-# Generating and printing a random number
 random_number = random.randint(1, 100)
 print(f"<p>Your random number is: {random_number}</p>")
 
-print("</body></html>")
