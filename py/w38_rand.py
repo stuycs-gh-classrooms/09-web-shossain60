b29e62a3fbc916ff87acba1ebad01b72655302cb
Shihab Hossain <shossain@stuy.edu>
2024-05-29 14:32:23 -0400

    +w38

--- py/w38_rand.py
+++ py/w38_rand.py
@@ -1,12 +1,10 @@
 <html>
 
 #!/usr/bin/python
-print('Content-type: text/html\n')
 
 import random
 
-random_number = random.randint(1, 1000000000000)
-print(f"<p>Your random number is: {random_number}</p>")
+print(f"<p>Your random number is: {random}</p>")
 
 
 </html>
